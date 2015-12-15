3e01b33ca03bb930c9db236b025ef771f77bf870
vinicius alves coutinho <viniciusdev@live.com>
2015-12-15 15:13:09 -0200

    nenhuma alteração grande

--- load/pdo.php
+++ load/pdo.php
@@ -1,18 +1,19 @@
 <?php
+/*
+	Pdo class
+*/
+class pdoclass{
 
-	class pdoclass{
-
-		private $connect = NULL;
-
-
-		public function connection(){
-			try{
-				$this->connect = new PDO("pgsql:host=localhost;dbname=point","postgres","123");
-				}
-				catch(PDOException $e){
-					exit($e->getMessage());
-				}
-				return $this->connect;
-			}
-		}	
+	private $connect = NULL;
+	
+	public function connection(){
+		try{
+			$this->connect = new PDO("pgsql:host=localhost;dbname=point","postgres","123");
+		}
+		catch(PDOException $e){
+			exit($e->getMessage());
+		}
+			return $this->connect;
+	}
+}	
 ?>
