084e941a8878baec215d5adf128e3c5559a289dc
vinicius alves coutinho <viniciusdev@live.com>
2015-12-15 14:56:49 -0200

    fixado bug

---
+++ load/pdo.php
@@ -0,0 +1,18 @@
+<?php
+
+	class pdoclass{
+
+		private $connect = NULL;
+
+
+		public function connection(){
+			try{
+				$this->connect = new PDO("pgsql:host=localhost;dbname=point","postgres","123");
+				}
+				catch(PDOException $e){
+					exit($e->getMessage());
+				}
+				return $this->connect;
+			}
+		}	
+?>
